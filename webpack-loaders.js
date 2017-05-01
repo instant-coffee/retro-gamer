dbe400a8fa921d2c3ea2f9a47100eae3884bbf91
Lachlan MacKenzie <lachlanrm@gmail.com>
2017-04-30 23:20:28 -0700

    Adding webpack loaders

---
+++ webpack-loaders.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const webpack = require('webpack');
+const PATHS = require('./webpack-paths');
+
+exports.devServer = options => {
+  return {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,  // Enable hot module
+      inline: true,
+      stats: 'errors-only',
+      host: options.host, // http://localhost
+      port: options.port, // 3000
+      contentBase: './client/dist',
+    },
+    // Enable multi-pass compilation for enhanced performance
+    plugins: [ // Hot module
+      new webpack.HotModuleReplacementPlugin({
+        multistep: true
+      })
+    ]
+  }
+};
+// the CSS loader
+exports.css = {
+  test: /\.css$/,
+  use: ['style-loader', 'css-loader'],
+  include: PATHS.css
+};
+
+// The font loader
+exports.font = {
+  test: /\.ttf$/,
+  use: ['file-loader']
+};
+
+//Babel loader
+exports.babel = {
+  test: /\.jsx?$/,
+  exclude: /node_modules/,
+  use: ['babel-loader']
+};
