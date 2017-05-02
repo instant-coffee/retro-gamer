fcfdda1839fc329185c915da9645c8b47d3bd604
Lachlan MacKenzie <lachlanrm@gmail.com>
2017-05-01 21:31:41 -0700

    Adding React routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,12 @@
-/**
- * Created by lachlanmackenzie on 2017-04-27.
- */
+import  '../dist/css/style.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Routes from './routes';
+
+filepicker.setKey("ANrfvAjhTLONfyN1UYi6wz");
+
+ReactDOM.render(
+  Routes,
+  document.getElementById('content')
+);
+
